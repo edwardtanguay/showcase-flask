723d0ffef6fc0c9d52e253fd45a352567a3d41de
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-22 21:03:16 +0200

    FEATURE: Add flashcards route

--- app/server.py
+++ app/server.py
@@ -6,5 +6,31 @@ app = Flask(__name__)
 def welcome():
 	return "Welcome to the Flask API"
 
-if __name__ == "__main__":
-	app.run(debug=True)
+@app.route("/flashcards")
+def flashcards():
+	return [
+    {
+        "suuid": "a6bmmF",
+        "category": "it",
+        "front": "and it will take some time",
+        "back": "e ci vorrà del tempo"
+    },
+    {
+        "suuid": "8m60CW",
+        "category": "it",
+        "front": "this will ensure that you get",
+        "back": "c'è garantirà di ottenere"
+    },
+    {
+        "suuid": "Q8JQnJ",
+        "category": "es",
+        "front": "this has certain advantages",
+        "back": "est tiene ciertas ventajas"
+    },
+    {
+        "suuid": "nFtvs8",
+        "category": "es",
+        "front": "in such a way that",
+        "back": "de tal manera que"
+    }
+]
